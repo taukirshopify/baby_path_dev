2119504a828ca0e4490231d07becde8cb26f1228
frsbmaza <frsbmaza2016ou@gmail.com>
2023-01-08 13:11:39 +0600

    update js

--- assets/theme.js
+++ assets/theme.js
@@ -264,3 +264,58 @@
     navbarFixed ();   
     bgImg();  
 })(jQuery);
+
+
+
+(function () {
+    "use strict";
+    var jQueryPlugin = (window.jQueryPlugin = function (ident, func) {
+        return function (arg) {
+        if (this.length > 1) {
+            this.each(function () {
+            var $this = $(this);
+
+            if (!$this.data(ident)) {
+                $this.data(ident, func($this, arg));
+            }
+            });
+
+            return this;
+        } else if (this.length === 1) {
+            if (!this.data(ident)) {
+            this.data(ident, func(this, arg));
+            }
+
+            return this.data(ident);
+        }
+        };
+    });
+    })();
+
+    (function () {
+    "use strict";
+    function Guantity($root) {
+        const element = $root;
+        const quantity = $root.first("data-quantity");
+        const quantity_target = $root.find("[data-quantity-target]");
+        const quantity_minus = $root.find("[data-quantity-minus]");
+        const quantity_plus = $root.find("[data-quantity-plus]");
+        var quantity_ = quantity_target.val();
+        $(quantity_minus).click(function () {
+        quantity_target.val(--quantity_);
+        });
+        $(quantity_plus).click(function () {
+        quantity_target.val(++quantity_);
+        });
+    }
+    $.fn.Guantity = jQueryPlugin("Guantity", Guantity);
+    $("[data-quantity]").Guantity();
+    })();
+    
+    // sidebar_menu All Page/
+    $(".sidebar-card").on('click', function () {
+        $(".main-card-area").toggleClass("active");
+    });
+    $(".main-card-remove").on('click', function () {
+        $(".main-card-area").removeClass("active");
+    });
