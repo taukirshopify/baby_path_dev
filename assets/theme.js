aabac53402a82523ba536ad1902fe4e0efd6ee93
frsbmaza <frsbmaza2016ou@gmail.com>
2023-01-10 00:11:27 +0600

    update

--- assets/theme.js
+++ assets/theme.js
@@ -156,6 +156,33 @@
 })(jQuery);
 
 
+
+(function () {
+    "use strict";
+    var jQueryPlugin = (window.jQueryPlugin = function (ident, func) {
+        return function (arg) {
+        if (this.length > 1) {
+            this.each(function () {
+            var $this = $(this);
+
+            if (!$this.data(ident)) {
+                $this.data(ident, func($this, arg));
+            }
+            });
+
+            return this;
+        } else if (this.length === 1) {
+            if (!this.data(ident)) {
+            this.data(ident, func(this, arg));
+            }
+
+            return this.data(ident);
+        }
+        };
+    });
+    })();
+
+
     // sidebar_menu All Page/
     $(".sidebar-card").on('click', function () {
         $(".main-card-area").addClass("active");
